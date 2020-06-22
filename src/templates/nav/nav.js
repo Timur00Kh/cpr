4cdcbf72b6ab47bb1876ea2e0192dc683f8472b6
Timur00Kh <newt300@mail.ru>
2020-06-22 18:38:54 +0300

    костыль мобильного навбара на iOS

--- src/templates/nav/nav.js
+++ src/templates/nav/nav.js
@@ -5,35 +5,44 @@ const mNav = (() => {
     const b = document.getElementById('nav_button');
     const m_nav = document.querySelector('.m_nav');
     const nav = document.querySelector('.nav');
+    const ios = iOS();
+
+    m_nav.addEventListener('animationend', () => {
+        if (!flag) {
+            nav.classList.add('shadow')
+        }
+    });
 
 
 
     return {
         open() {
-            document.querySelector('html').style.overflow = 'hidden';
+            document.querySelector('html').classList.add('lock');
+            document.querySelector('body').classList.add('lock');
 
-            b.classList.remove('nav__menu');
-            b.classList.add('nav__close');
+            b.classList.remove('nav__menu', !ios && 'nav__menu--animated');
+            b.classList.add('nav__close', !ios && 'nav__close--animated');
 
             m_nav.classList.remove('d-none', 'm_nav--closed');
             m_nav.classList.add('m_nav--active');
             flag = true;
 
-            nav.classList.remove('shadow')
+            if (window.innerHeight > 680 ) {
+                nav.classList.remove('shadow')
+            }
 
         },
         close() {
-            document.querySelector('html').style.overflow = '';
+            document.querySelector('html').classList.remove('lock');
+            document.querySelector('body').classList.remove('lock');
 
-            b.classList.add('nav__menu');
-            b.classList.remove('nav__close');
+            b.classList.add('nav__menu', !ios && 'nav__menu--animated');
+            b.classList.remove('nav__close', !ios && 'nav__close--animated');
 
             m_nav.classList.remove('m_nav--active');
             m_nav.classList.add('m_nav--closed');
             // setTimeout(() => m_nav.classList.add('d-none'), 500);
             flag = false;
-
-            setTimeout(() => nav.classList.add('shadow'), 200);
         }
     }
 })();
@@ -76,3 +85,23 @@ window.addEventListener('resize', () => {
         mNav.close()
     }
 });
+
+function iOS() {
+
+    var iDevices = [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
+    ];
+
+    if (navigator.platform) {
+        while (iDevices.length) {
+            if (navigator.platform === iDevices.pop()){ return true; }
+        }
+    }
+
+    return false;
+}
